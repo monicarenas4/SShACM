262f5ae15c072ab5d81ec7da35a5f4369961a2ee
monicarenas4 <monicarenas4@gmail.com>
2020-11-24 11:00:59 +0100

    main

--- mainProcessing.py
+++ mainProcessing.py
@@ -10,22 +10,22 @@ today = str(datetime.datetime.today())
 
 refPath = 'referenceImages'
 results_Path = 'results'
-type = 'mutatedImages'
+type = 'retakenImages'
 txtFile = results_Path + '/' + today[:10] + '_' + type + '.txt'
 
 referenceFolder = [f for f in listdir(refPath) if isfile(join(refPath, f))]
 
 for refFolder in referenceFolder:
     folderName = type + '/' + refFolder[:-4]
-    referenceImage = [f for f in listdir(folderName) if isfile(join(folderName, f))]
+    retakeImages = [f for f in listdir(folderName) if isfile(join(folderName, f))]
 
-    for m in range(0, len(referenceImage)):
+    for m in range(0, len(retakeImages)):
         start = time.perf_counter()
-        challengeIm = referenceImage[0]
-        maskedscore(folderName + '/' + challengeIm, folderName + '/' + referenceImage[m])
-        alignScore(folderName + '/' + challengeIm, folderName + '/' + referenceImage[m], colorthreshold)
+        challengeIm = refFolder
+        maskedscore(folderName + '/' + challengeIm, folderName + '/' + retakeImages[m])
+        alignScore(folderName + '/' + challengeIm, folderName + '/' + retakeImages[m], colorthreshold)
         finish = time.perf_counter()
-        print(m, referenceImage[m], f'{finish - start:0.2f}', f'{finish:0.2f}')
+        print(retakeImages[m], f'{finish - start:0.2f}', f'{finish:0.2f}')
 
         # with open(txtFile, 'a') as results_file:
         #     results_file.write(str(m + 1) + '\t' + f'{finish - start:0.2f}' + '\t')
