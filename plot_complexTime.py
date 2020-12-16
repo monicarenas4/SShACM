a87b4fa63906dc54e2d0c3bc1f825582c6a8ba1f
monicarenas4 <monicarenas4@gmail.com>
2020-12-16 20:57:49 +0100

    .

---
+++ plot_complexTime.py
@@ -0,0 +1,37 @@
+from itertools import cycle
+from numpy import cumsum
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+cores = 8
+setValue = 3
+rng1 = ['1', '2', '3', '4']
+rng = range(4, 34, 4)
+lines = ["-", "--", "-.", ":"]
+linecycler = cycle(lines)
+
+file_complex = 'results/2020-12-16_localFunction_Images.txt'
+df_complex = pd.read_csv(file_complex, delimiter='\t')
+time_per_image = (df_complex['time']).to_numpy()
+val1 = round((cumsum(time_per_image[:setValue+1]))[-1], 2)
+val2 = round((cumsum(time_per_image[28:32]))[-1] / cores, 2)
+
+a = 0
+for ln in range(len(rng)):
+    cpu_time = (cumsum(time_per_image[a:rng[ln]]) / (ln + 1))
+    plt.plot(cpu_time, next(linecycler), label='CPUs = ' + str(ln + 1))
+    a = rng[ln]
+
+plt.axvline(x=setValue, color='red', linestyle='-.')
+plt.xlim([0, 3])
+plt.ylim([0, 28])
+plt.annotate(str(val1), xy=(setValue, val1))  # , xytext=(setValue, 530))
+plt.annotate(str(val2), xy=(setValue, val2)) #, xytext=(setValue, 75))
+plt.grid()
+plt.xticks(range(len(rng1)), rng1)
+plt.xlabel('Problem size (# tile images)', fontsize=12)
+plt.ylabel('Time (s)', fontsize=12)
+plt.legend(fontsize=12)
+plt.title('Time Complexity for the local function')
+plt.show()
---
+++ plt_exTime.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+global_time = 'results/2020-12-15_globalFunction_Time.txt'
+local_time = 'results/2020-12-15_localFunction_Time.txt'
+
+df_glob = pd.read_csv(global_time, delimiter='\t')
+df_loc = pd.read_csv(local_time, delimiter='\t')
+cpu_glob = df_glob['cpus']
+cpu_loc = df_loc['cpus']
+time_glob = df_glob['time'] / 40
+time_loc = df_loc['time']
+
+plt.plot(cpu_glob, time_glob,
+         linestyle='--', marker='o', color='tab:red',
+         label='Global function')
+plt.plot(cpu_loc, time_loc,
+         linestyle='-.', marker='*', color='tab:blue',
+         label='Local function')
+plt.xlim([1, 8])
+plt.grid()
+plt.xlabel('Number of CPUs', fontsize=12)
+plt.ylabel('Time (s)', fontsize=12)
+plt.legend(fontsize=12)
+plt.title('Execution time depending on the number of cores \n'
+          'for a single image')
+plt.show()
